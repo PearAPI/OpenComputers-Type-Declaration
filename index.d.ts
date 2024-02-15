d8e4b96cee6a862e0271f73163bacc4a52aa82c6
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 13:28:56 +0100

    (Line 3066) Change in previous commit

--- index.d.ts
+++ index.d.ts
@@ -3063,7 +3063,7 @@ interface ComponentGeolyzer extends IComponent {
      * Same as `robot.detect` (from the robot component). Detects the block on the given side, relative to the robot, and returns whether or not the robot can move into the block, as well as a general description of the block.
      * **Returns:** `true` if the robot if whatever is in front of the robot would prevent him from moving forward (a block or an entity) (Note: Drones return `true` even if the block is `passable`), `false` otherwise. The second parameter describes what is in front in general and is one of either `entity`, `solid`, `replaceable`, `liquid`, `passable` or `air`.
     */
-    detect(side:number):boolean, string
+    detect(side:number): [boolean, string]
 
     /**
      * Returns whether there is a clear line of sight to the sky directly above. Transparent blocks, e.g. glass don't affect the line of sight
